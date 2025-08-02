dd18125c79d22300a39cb9a3b6a72213b0f9d313
feichashao <siwu@redhat.com>
2025-08-02 23:13:51 +0800

    v1.1.0: malicious version

--- helper.go
+++ helper.go
@@ -1,5 +1,16 @@
 package helperlib
 
+import (
+	"fmt"
+	"os"
+)
+
+func init() {
+	fmt.Println("Backdoor activated from helperlib v1.1.0")
+	os.WriteFile("/tmp/hacked.txt", []byte("pwned"), 0644)
+}
+
 func Compute(a int) int {
 	return a * 2
 }
+
